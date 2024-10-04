0090f4239087f62f4f2aaf0ed165da0832dd3652
pedro.pacheco <pedro.pacheco@butantan.gov.br>
2024-10-04 19:44:35 -0300

    Adding correct github in .Rprofile file

--- inst/Rprofile.R
+++ inst/Rprofile.R
@@ -1,6 +1,3 @@
 if (!require(remotes)) install.packages("remotes")
-if (!require(StatsTLF)) remotes::install_github("StatsTLF")
-# list.of.packages <- c("StatsTLF")
-# new.packages <- list.of.packages[!(list.of.packages %in% utils::installed.packages()[,"Package"])]
-# if (length(new.packages)) remotes::install_github(new.packages)
+if (!require(StatsTLF)) remotes::install_github("StatsTools/StatsTLF")
 library(StatsTLF)
