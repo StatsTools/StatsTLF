11dbad330c44d6fda9db3c79d88b8b4a6e923a7b
Lucas Avila Moreira De Paula <lucas.paula>
2025-09-23 15:39:44 -0300

    colocando a função que deriva a forma numérica de todas as variáveis do banco.

---
+++ R/method_numcat.R
@@ -0,0 +1,17 @@
+mtnumcat <- function(dataset){
+  j <- 1
+  for(i in colnames(dataset)){
+    #if(attr(dataset[[i]], "method_name") == "Algorithm to derive numeric code from categorical variable"){
+    if(stringr::str_sub(attr(dataset[[i]], "label"), -4, -1) == " (N)"){
+      dataset <- dataset |>
+        derive2(var_target = !!colnames(dataset)[j],
+                var_source = !!colnames(dataset)[j-1],
+                by = rlang::exprs(USUBJID))
+    }
+
+    j <- j + 1
+  }
+  return(dataset)
+}
+
+vaicorinthia <- mtnumcat(adsl)
